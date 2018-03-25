689268992325cde0bcf916efad0a5d4cedcff873
Faris Sbahi <fms15@duke.edu>
2018-03-25 17:02:51 -0400

    flags

--- src/MatchingEngine.go
+++ src/MatchingEngine.go
@@ -3,7 +3,23 @@ package main
 import (
 	"./tcp_server"
 	"github.com/golang/glog"
+	"os"
+	"flag"
+	"fmt"
 	)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: example -stderrthreshold=[INFO|WARN|FATAL] -log_dir=[string]\n", )
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func init() {
+	flag.Usage = usage
+	// NOTE: This next line is key you have to call flag.Parse() for the command line 
+	// options or "flags" that are defined in the glog module to be picked up.
+	flag.Parse()
+}
 
 func main() {
 	server := tcp_server.New("127.0.0.1:12345")
@@ -12,6 +28,7 @@ func main() {
 		// New Client Connected
 		glog.Info("New client connection")
 		c.Send("Hello")
+		glog.Flush()
 	})
 	server.OnNewMessage(func(c *tcp_server.Client, message string) {
 		// New Message Received
