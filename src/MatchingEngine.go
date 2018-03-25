03e96d05a2c6a4dfa4faee45dee0831ad1979c30
farice <farissbahi@gmail.com>
2018-03-25 16:22:20 -0400

    stuff

--- src/MatchingEngine.go
+++ src/MatchingEngine.go
@@ -1,20 +1,25 @@
 package main
 
-import "./tcp_server"
+import (
+	"./tcp_server"
+	"github.com/golang/glog"
+	)
 
 func main() {
-	server := tcp_server.New("localhost:12345")
+	server := tcp_server.New("127.0.0.1:12345")
 
 	server.OnNewClient(func(c *tcp_server.Client) {
-		// new client connected
-		// lets send some message
+		// New Client Connected
+		glog.Info("New client connection")
 		c.Send("Hello")
 	})
 	server.OnNewMessage(func(c *tcp_server.Client, message string) {
-		// new message received
+		// New Message Received
+		glog.Info("New message received")
 	})
 	server.OnClientConnectionClosed(func(c *tcp_server.Client, err error) {
-		// connection with client lost
+		// Lost connection with Client
+		glog.Info("New client connection")
 	})
 
 	server.Listen()
