a8c6b8e05470cadfa773e2b9b54777c05310c96b
farice <farissbahi@gmail.com>
2018-03-26 01:49:43 -0400

    parse create XML

--- src/request_handler.go
+++ src/request_handler.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
+  "encoding/xml"
 )
+
+// Remember to capitalize field names so they are exported 
+type Create struct {
+	XMLName xml.Name `xml:"create"`
+  Accounts []struct {
+    Id string `xml:"id,attr"`
+    Balance string `xml:"balance,attr"`
+  } `xml:"account"`
+
+  Symbols []struct {
+    Sym string `xml:"sym,attr"`
+    Accounts []struct {
+      Id string `xml:"id,attr"`
+      Amount string `xml:",innerxml"`
+    } `xml:"account"`
+
+  } `xml:"symbol"`
+
+}
 
 // Send bytes to Connection
 func (c *Connection) handleRequest(req []byte) {
   // New Message Received
+  var createXML Create
+  err := xml.Unmarshal(req, &createXML)
+
+  if err != nil {
+        log.Error("Error unmarshalling from XML", err)
+        return
+  }
+
   log.WithFields(log.Fields{
-  "message": string(req),
+  "XML": createXML,
 }).Info("New message received")
+
 }
