4c6ae42fadff0c64563a9dffdd242117fd5836b5
andrewbihl <andrewbihlva@gmail.com>
2018-04-04 19:26:06 -0400

    Automate key generation.

--- src/exchange_db.sql
+++ src/exchange_db.sql
@@ -1,22 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS account (
-    uid varchar PRIMARY KEY,
+    uid uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
     balance float
 );
 CREATE TABLE IF NOT EXISTS position (
-    uid varchar PRIMARY KEY,
+    uid uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
     account_id varchar,
     symbol varchar,
     amount float
 );
 CREATE TABLE IF NOT EXISTS buy_order (
-    uid varchar PRIMARY KEY,
+    uid uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
     account_id varchar,
     symbol varchar,
     limit float,
     amount float
 );
 CREATE TABLE IF NOT EXISTS sell_order (
-    uid varchar PRIMARY KEY,
+    uid uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
     account_id varchar,
     symbol varchar,
     limit float,
