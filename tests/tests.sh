2c6fb4f553380ee255065d85b5c7cbc1f797cb3c
Faris Sbahi <fms15@oit-link02.oit.duke.edu>
2018-04-05 18:29:38 -0400

    More interesting test cases

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,8 +3,8 @@ echo Testing Server Availability
 echo "hello" | nc localhost 12345
 cat ../logs/exchange.log
 
-echo Testing Sample Request
-cat sample_request.txt | nc localhost 12345
+echo Testing Sample Create
+cat sample_create.txt | nc localhost 12345
 cat ../logs/exchange.log
 
 echo Stress test with Sample Request
@@ -12,12 +12,19 @@ seq 10 | parallel -n0 "cat sample_create.txt | nc localhost 12345"
 cat ../logs/exchange.log
 
 echo Testing Sample Transactions
-cat sample_transactions.txt | nc localhost 12345
+cat sample_buys.txt | nc localhost 12345 && cat sample_sells.txt | nc localhost 12345
 cat ../logs/exchange.log
 
 echo Stress test with Sample Transactions
-seq 10 | parallel -n0 "cat sample_buys.txt | nc localhost 12345"
-seq 10 | parallel -n0 "cat sample_sells.txt | nc localhost 12345"
+seq 10 | parallel -n0 "cat sample_buys.txt | nc localhost 12345 && cat sample_sells.txt | nc localhost 12345"
+cat ../logs/exchange.log
+
+echo Testing Sample Create + Transactions
+cat sample_create.txt | nc localhost 12345 && cat sample_sells.txt | nc localhost 12345 && cat sample_buys.txt | nc localhost 12345
+cat ../logs/exchange.log
+
+echo Stress test with Create/Transactions
+seq 10 | parallel -n0 "cat sample_create.txt | nc localhost 12345 && cat sample_sells.txt | nc localhost 12345 && cat sample_buys.txt | nc localhost 12345"
 cat ../logs/exchange.log
 
 echo Conclude test
