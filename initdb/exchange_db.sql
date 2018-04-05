5100ad0d950374e05f0fb32c8ccad4d18871e446
farice <farissbahi@gmail.com>
2018-04-05 03:29:39 -0400

    rename

--- initdb/exchange_db.sql
+++ initdb/exchange_db.sql
@@ -13,14 +13,14 @@ CREATE TABLE IF NOT EXISTS buy_order (
     uid varchar PRIMARY KEY,
     account_id varchar,
     symbol varchar,
-    limit_f float,
+    price_limit float,
     amount float
 );
 CREATE TABLE IF NOT EXISTS sell_order (
     uid varchar PRIMARY KEY,
     account_id varchar,
     symbol varchar,
-    limit_f float,
+    price_limit float,
     amount float
 );
 CREATE TABLE IF NOT EXISTS symbol (
@@ -33,5 +33,5 @@ CREATE TABLE IF NOT EXISTS transaction (
     price float,
     transaction_time time
 );
-CREATE INDEX buy_limit ON buy_order (limit_f);
-CREATE INDEX sell_limit ON sell_order (limit_f);
+CREATE INDEX buy_limit ON buy_order (price_limit);
+CREATE INDEX sell_limit ON sell_order (price_limit);
