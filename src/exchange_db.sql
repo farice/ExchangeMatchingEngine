297ca32ece42d5a5a637c46badcf8ea849e49e42
andrewbihl <andrewbihlva@gmail.com>
2018-04-05 01:20:25 -0400

    Fix .sql script to not generate uuids from postgres.

--- src/exchange_db.sql
+++ src/exchange_db.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS account (
     uid varchar PRIMARY KEY,
     balance float
@@ -11,14 +9,14 @@ CREATE TABLE IF NOT EXISTS position (
     PRIMARY KEY(account_id, symbol)
 );
 CREATE TABLE IF NOT EXISTS buy_order (
-    uid uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
+    uid varchar PRIMARY KEY,
     account_id varchar,
     symbol varchar,
     limit float,
     amount float
 );
 CREATE TABLE IF NOT EXISTS sell_order (
-    uid uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
+    uid varchar PRIMARY KEY,
     account_id varchar,
     symbol varchar,
     limit float,
