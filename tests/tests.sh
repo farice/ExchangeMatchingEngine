64601ae74aa1d19801fbd4b6124f1ff5ec67cdeb
farice <farissbahi@gmail.com>
2018-04-06 16:03:28 -0400

    add rm to tests script

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,29 +1,36 @@
 #!/usr/bin/env bash
 echo Testing Server Availability
+rm ../logs/exchange.log
 echo "hello" | nc localhost 12345
 cat ../logs/exchange.log
 
 echo Testing Sample Create
+rm ../logs/exchange.log
 cat sample_create.txt | nc localhost 12345
 cat ../logs/exchange.log
 
 echo Stress test with Sample Request
+rm ../logs/exchange.log
 seq 10 | parallel -n0 "cat sample_create.txt | nc localhost 12345"
 cat ../logs/exchange.log
 
 echo Testing Sample Transactions
+rm ../logs/exchange.log
 cat sample_buy_1.txt | nc localhost 12345 && cat sample_sell_1.txt | nc localhost 12345
 cat ../logs/exchange.log
 
 echo Stress test with Sample Transactions
+rm ../logs/exchange.log
 seq 10 | parallel -n0 "cat sample_buy_1.txt | nc localhost 12345 && cat sample_sell_1.txt | nc localhost 12345"
 cat ../logs/exchange.log
 
 echo Testing Sample Create + Transactions
+rm ../logs/exchange.log
 cat sample_create.txt | nc localhost 12345 && cat sample_sell_1.txt | nc localhost 12345 && cat sample_buy_1.txt | nc localhost 12345
 cat ../logs/exchange.log
 
 echo Stress test with Create/Transactions
+rm ../logs/exchange.log
 seq 10 | parallel -n0 "cat sample_create.txt | nc localhost 12345 && cat sample_sell_1.txt | nc localhost 12345 && cat sample_buy_1.txt | nc localhost 12345"
 cat ../logs/exchange.log
 
