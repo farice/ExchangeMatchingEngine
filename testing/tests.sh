ede4ad3d516245ad1fec9bedea4b62e2c0b5b7b7
farice <farissbahi@gmail.com>
2018-04-20 11:34:57 -0400

    more tests

--- testing/tests.sh
+++ testing/tests.sh
@@ -11,12 +11,36 @@ echo Stress test with Sample Request
 seq 10 | parallel -n0 "cat create/sample.txt | nc localhost 12345"
 cat ../logs/exchange.log
 
-echo Testing Sample Transactions
+echo Testing Sample Buy
 cat transaction/buy/1.txt | nc localhost 12345 && cat transaction/sell/1.txt | nc localhost 12345
 cat ../logs/exchange.log
 
-echo Stress test with Sample Transactions
+echo Testing Sample Sell
+cat transaction/sell/1.txt | nc localhost 12345 && cat transaction/sell/1.txt | nc localhost 12345
+cat ../logs/exchange.log
+
+echo Testing Sample Buy
+cat transaction/buy/1.txt | nc localhost 12345 && cat transaction/sell/1.txt | nc localhost 12345
+cat ../logs/exchange.log
+
+echo Testing Sample Cancel
+cat transaction/cancel/1.txt | nc localhost 12345 && cat transaction/sell/1.txt | nc localhost 12345
+cat ../logs/exchange.log
+
+echo Testing Sample Query
+cat transaction/query/1.txt | nc localhost 12345 && cat transaction/sell/1.txt | nc localhost 12345
+cat ../logs/exchange.log
+
+echo Stress test with Sample Buys
 seq 10 | parallel -n0 "cat transaction/buy/1.txt | nc localhost 12345 && cat transaction/sell/1.txt | nc localhost 12345"
+cat ../logs/exchange.log
+
+echo Stress test with Sample Sells
+seq 10 | parallel -n0 "cat transaction/buy/1.txt | nc localhost 12345 && cat transaction/sell/1.txt | nc localhost 12345"
+cat ../logs/exchange.log
+
+echo Stress test with Sample Cancels
+seq 10 | parallel -n0 "cat transaction/cancel/2.txt | nc localhost 12345 && cat transaction/sell/1.txt | nc localhost 12345"
 cat ../logs/exchange.log
 
 echo Testing Sample Create + Transactions
